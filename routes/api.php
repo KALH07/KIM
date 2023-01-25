03294e803c3507048bcb1d7cd03af33fe9882707
KALH <traorealhassane810@gmail.com>
2023-01-25 17:47:09 +0000

    Creer controleur pour appeler des methodes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
